2b6107f27a3a3e1b6531d417378e2f661e1958ca
rxxed <raaed@tuta.io>
2021-02-10 07:56:56 +0000

    formatting

--- src/scrape.py
+++ src/scrape.py
@@ -1,24 +1,27 @@
-import time
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+URL = 'https://mdcomputers.in'
+SEARCH_TERM = 'Ryzen 3 2200G'
+
 driver = webdriver.Firefox()
-driver.get('https://mdcomputers.in')
+driver.get('URL')
 
 search_elem = driver.find_search_element_by_name('search')
 search_elem.clear()
-search_elem.send_keys('Ryzen 3 2200G')
+search_elem.send_keys(SEARCH_TERM)
 search_elem.send_keys(Keys.RETURN)
 
 # required as we have to wait for the search results to load
-time.sleep(5)
+sleep(5)
 
 product = driver.find_element_by_class_name('product-image-container')
 product.click()
 
 # wait for the product page to load
-time.sleep(5)
+sleep(5)
 
 price = driver.find_element_by_id('price-old')
 
