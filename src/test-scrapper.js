e8b3f6d13795fa20af561b58b106985e1d6a8e3a
Muhammad Althaf Rahman <muhammadalthafrahman@gmail.com>
2021-02-19 19:18:21 +0530

    test this in browser console

---
+++ src/test-scrapper.js
@@ -0,0 +1,45 @@
+/* Amazon */
+
+//product container
+console.log(document.getElementsByClassName("a-section a-spacing-medium"));
+
+// product name
+console.log(document.getElementsByClassName("a-size-mini a-spacing-none a-color-base s-line-clamp-2"))
+
+// product price
+console.log(document.getElementsByClassName("a-size-base a-link-normal s-no-hover a-text-normal"))
+
+/***************************************************************************************************/
+/* mdcomputer*/
+
+//product container
+console.log(document.getElementsByClassName("product-layout product-grid product-grid-5 col-lg-15 col-md-4 col-sm-6 col-xs-12"))
+
+// product name
+console.log(document.getElementsByClassName("right-block right-b"));
+
+// product price
+console.log(document.getElementsByClassName("price"));
+
+/*******************************************************************************/
+/*vedant computer*/
+
+//product container
+console.log(document.getElementsByClassName("product-layout hide-cart out-of-stock has-extra-button"));
+
+// product name
+console.log(document.getElementsByClassName("name"));
+
+// product price
+console.log(document.getElementsByClassName("price"));
+
+/****************************************************************************/
+/* theitdepot*/
+
+// product container
+console.log(document.getElementsByClassName("product-item col-6 col-md-4 col-lg-3 p-1"));
+
+// product name
+console.log(document.getElementsByClassName("card-text px-2 py-1 font-size85 product_title"));
+
+// product price
