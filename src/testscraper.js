afa1dc450f13f674c015cd04cef3ca22b17e8cba
Muhammad Althaf Rahman <muhammadalthafrahman@gmail.com>
2021-02-25 21:48:05 +0530

    we shall rewrite everything

--- src/testscraper.js
+++ src/testscraper.js
@@ -1,40 +1,39 @@
-const puppeteer = require('puppeteer');
+/*vedantcomputer*/
+let product_items = document.getElementsByClassName("product-thumb");
+for(i = 0; i <= product_items.length; i++) {
+	if(product_items[i].querySelector(".price-new")){
+		price = product_items[i].querySelector(".price-new").textContent
+	}
+	else  {
+		price = product_items[i].querySelector(".price").textContent
+	}
+	console.log(product_items[i].querySelector(".name").textContent)
+	console.log(product_items[i].querySelector(".name > a").href)
+	console.log(price);
+}
+/************************************************************/
+/*theitdepot*/
+let product_items = document.getElementsByClassName("product-item");
+for(i = 0; i < product_items.length; i++) {
+	console.log(product_items[i].querySelector("div.product-details.text-md-left.flex-grow-1 > div.card-text.px-2.py-1.font-size85.product_title > a").textContent);
+	console.log(product_items[i].querySelector("div.product-details.text-md-left.flex-grow-1 > div.card-text.px-2.py-1.font-size85.product_title > a").href);
+	console.log(product_items[i].querySelector("strong").textContent);
+}
+/******************************************************************************/
+/*primeadgb*/
+let product_items = document.getElementsByClassName("product-item");
+for(i = 0; i < product_items.length; i++) {
+	console.log(product_items[i].querySelector("div.product-innfo > h3 > a").textContent);
+	console.log(product_items[i].querySelector("div.product-innfo > h3 > a").href);
+	console.log(product_items[i].querySelector("div.product-innfo > span > ins > span > bdi").textContent);
+}
 
-(async () => {
-	const extractProducts = async url => {
-		const browser = await puppeteer.launch({headless: false});
-		const page = await browser.newPage();
-		await page.goto(url);
-		const results = await page.evaluate(() => {
-			let products = []
-			let prd_titles = document.getElementsByClassName("right-block right-b");
-			let prd_prices = document.getElementsByClassName("price-new");
-			if(prd_titles.length == prd_prices.length) {
-				for(i = 0; i < prd_titles.length; i++) {
-					products.push(
-						{
-							'title': prd_titles[i].querySelector("h4 a").textContent.replace(/\t|\n/g,''),
-							'url': prd_titles[i].querySelector("h4 a").href,
-							'price': prd_prices[i].textContent.replace(/\t|\n/g,'')
-						})
-				}
-			}
-			return products
-		});
-		await page.close();
-		await browser.close();
-		if(results.length < 1){
-			return results
-		} else {
-			const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
-			const nextUrl = `https://mdcomputers.in/memory?page=${nextPageNumber}`;
-			return results.concat(await extractProducts(nextUrl))
-		}
-	};
-
-	const browser = await puppeteer.launch();
-	const firstUrl =
-		"https://mdcomputers.in/memory?page=1";
-	const prds = await extractProducts(firstUrl);
-	console.log(prds);
-})();
+/******************************************************************************/
+/*amazon*/
+	let product_items = document.getElementsByClassName("sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col sg-col-4-of-20");
+for(i = 0; i < product_items.length; i++) {
+console.log(product_items[i].querySelector("h2 > a").textContent.replace(/\t|\n/g,''))
+console.log(product_items[i].querySelector("h2 > a").href)
+console.log(product_items[i].querySelector(".a-price-whole").textContent)
+}
+/******************************************************************************/
