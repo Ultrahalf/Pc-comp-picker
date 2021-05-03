bd5d2b365abdf658b6d1b80b22e5ac934ee4ca55
Muhammad Althaf Rahman <muhammadalthafrahman@gmail.com>
2021-05-03 14:30:25 +0530

    adds recursion for the vedant scraper

--- src/node/vedant.js
+++ src/node/vedant.js
@@ -4,45 +4,49 @@ var dbUrl = "mongodb://localhost:27017/";
 var dbName = "pccomppicker";
 
 (async () => {
-	const extractProducts = async url => {
-		const browser = await puppeteer.launch({headless: false});
-		const page = await browser.newPage();
-		await page.goto(url);
-		await autoScroll(page);
-		const results = await page.evaluate(() => {
+    const extractProducts = async url => {
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+        await page.goto(url);
+        const results = await page.evaluate(() => {
             let vendor = "vendant";
-			let products = [];
-			let product_items = document.querySelectorAll(".product-thumb");
-			let len = product_items.length
-			for(i = 0; i <= len - 1; i++) {
-				if(product_items[i].querySelector(".price")) {
-					if(product_items[i].querySelector(".price-new")){
-						price = product_items[i].querySelector(".price-new").textContent
-					}
-					else  {
-						price = product_items[i].querySelector(".price").textContent
-					}
-				}
-				products.push(
-					{
+            let products = [];
+            let product_items = document.querySelectorAll(".product-thumb");
+            let len = product_items.length
+            for(i = 0; i <= len - 1; i++) {
+                if(product_items[i].querySelector(".price")) {
+                    if(product_items[i].querySelector(".price-new")){
+                        price = product_items[i].querySelector(".price-new").textContent
+                    }
+                    else  {
+                        price = product_items[i].querySelector(".price").textContent
+                    }
+                }
+                products.push(
+                    {
                         'vendor': vendor,
-						'title': product_items[i].querySelector(".name").textContent,
-						'img': product_items[i].querySelector("img").src,
-						'url': product_items[i].querySelector(".name > a").href,
-						'price': price.replace(/\t|\n/g,''),
-					})
-			}
-			return products
-			// return len
-		});
-		await page.close();
-		await browser.close();
-		return results
-	};
+                        'title': product_items[i].querySelector(".name").textContent,
+                        'img': product_items[i].querySelector("img").src,
+                        'url': product_items[i].querySelector(".name > a").href,
+                        'price': price.replace(/\t|\n/g,''),
+                    })
+            }
+            return products
+        });
+        await page.close();
+        await browser.close();
+        if (results.length < 1) {
+            return results
+        } else {
+            const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
+            const nextUrl = `https://www.vedantcomputers.com/pc-components/graphics-card?fq=1&page=${nextPageNumber}`;
+            return results.concat(await extractProducts(nextUrl))
+        }
+    };
 
-	const browser = await puppeteer.launch();
-	const firstUrl = "https://www.vedantcomputers.com/pc-components/graphics-card"
-	const prds = await extractProducts(firstUrl);
+    const browser = await puppeteer.launch();
+    const firstUrl = "https://www.vedantcomputers.com/pc-components/graphics-card?fq=1&page=1";
+    const prds = await extractProducts(firstUrl);
     let client;
     try {
         client = await MongoClient.connect(dbUrl);
@@ -56,26 +60,5 @@ var dbName = "pccomppicker";
         console.log(err.stack);
     }
     client.close();
-	process.exit();
+    process.exit();
 })();
-
-async function autoScroll(page){
-	await page.evaluate(async () => {
-		await new Promise((resolve, reject) => {
-			var totalHeight = 0;
-			var distance = 20;
-			var timer = setInterval(() => {
-				var scrollHeight = document.body.scrollHeight;
-				window.scrollBy(0, distance);
-				totalHeight += distance;
-
-				if(totalHeight >= scrollHeight){
-					clearInterval(timer);
-					resolve();
-				}
-				if(document.querySelector(".ias-trigger-next a"))
-					document.querySelector(".ias-trigger-next > a.btn").click();
-			}, 100);
-		});
-	});
-}
