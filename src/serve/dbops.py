fcf2fa34dbbf5515cc9d2fc65e3244cc21e714ac
rxxed <raaed@tuta.io>
2021-06-19 12:49:59 +0530

    Rudimentary wishlist

--- src/serve/dbops.py
+++ src/serve/dbops.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
 
-import mongoengine
-from mongoengine import connect
+import sys
+import json
+from bson.objectid import ObjectId
 
-connect('pccomppicker')
+from pymongo import MongoClient
 
-class Product(mongoengine.Document):
-    vendor = mongoengine.StringField(required=True)
-    title = mongoengine.StringField(required=True)
-    img = mongoengine.StringField(required=True)
-    url = mongoengine.StringField(required=True)
-    price = mongoengine.StringField(required=True)
-    category = mongoengine.StringField(required=True)
+mc = MongoClient('localhost', 27017)
+db = mc['pccomppicker']
+products = db['products']
 
-    meta = {
-        'collection': 'products',
-    }
+
+class JSONEncoder(json.JSONEncoder):
+    
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        return super(JSONEncoder, self).default(obj)
+
+
+def get_data(pagelen: int, category: str):
+    objects = products.find({'category': category}).limit(pagelen)
+
+    list_of_dicts = list()
+    for obj in objects:
+        list_of_dicts.append(obj)
+
+    return list_of_dicts
+
+
+def get_product_with_id(product_id):
+    product = products.find_one({'_id': ObjectId(product_id)})
+    if product == None:
+        print("No such product id!")
+        sys.exit(1)
+    return product
+
+
+if __name__ == '__main__':
+    prod = get_product_with_id('60ccb5ffc3aa12d41a9a9b1b')
+    print(prod['title'])
